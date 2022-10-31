129248358510e8ca60f6fce0bae4aec4c77c92e3
Yarik <117121382+demurre@users.noreply.github.com>
2022-10-31 20:55:19 +0000

    Add files via upload

---
+++ practice/LinearComputationalProcess/pr2v2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	double m1, m2, r, F;
+	double const g = 6.673 * pow(10, 11);
+	cout << "Enter m1: ";
+	cin >> m1;
+	cout << "Enter m2: ";
+	cin >> m2;
+	cout << "Enter r: ";
+	cin >> r;
+	F = g * (m1 * m2 / (r * r));
+	cout << "F= "<<fixed <<F <<endl;
+	return 0;
+}
