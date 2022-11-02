8553d4db0ea601737378ff6a8bbb60b53eba0804
Yarik <117121382+demurre@users.noreply.github.com>
2022-11-02 11:21:58 +0000

    Add files via upload

---
+++ practice/Cycles/pr4(2).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+int main() {
+	const double PI = acos(-1.0);
+	const double xmin = PI / 4, xmax = PI / 2;
+	const int m = 15;
+	const double h = (xmax - xmin) / m;
+	cout << "x\ty" << endl;
+	double x = xmin;
+	while (x <= xmax) {
+		if (x <= 0) {
+			cout << fixed << setprecision(2) << x << "\t" << "no" << endl;
+		}
+		else {
+			double y = sin(x);
+			cout << fixed << setprecision(2) << x << "\t" << y << endl;
+		}
+		x += h;
+	}
+	return 0;
+}
---
+++ practice/Cycles/pr4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+int main(){
+	const double PI = acos(-1.0);
+	const double xmin=PI/4, xmax=PI/2;
+	const int m = 15;
+	const double h = (xmax - xmin) / m;
+	cout << "x\ty" << endl;
+	for (double x = xmin; x <= xmax; x += h) {
+		if (x <= 0) {
+			cout << fixed << setprecision(2) << x << "\t" << "no" << endl;
+		}
+		else {
+			double y = sin(x);
+			cout << fixed << setprecision(2) << x << "\t" << y << endl;
+		}
+	}
+	return 0;
+}
