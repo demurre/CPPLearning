32b145ad0f5253fd14661c777eee4962e30b33a4
Yarik <117121382+demurre@users.noreply.github.com>
2022-11-04 15:28:23 +0000

    Add files via upload

---
+++ practice/WorkWithPointersAndArrays/pr8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+void swap(int* x, int n){ //function to change elements
+    int i = 0, temp = 0;
+    for (i = 0; i < n / 2; i++){
+        temp = x[i];
+        x[i] = x[n - i - 1];
+        x[n - i - 1] = temp;
+    }
+}
+int main() {
+    const int n = 10;
+    int x[n];
+    cout << "Array: ";
+    for (int i = 0; i < n; ++i) {
+        x[i] = rand() % 10;
+        cout << x[i] << " ";
+    }
+    swap(x, n);
+        cout << endl << "New Array: ";
+        for (int i = 0; i < n; ++i)
+        cout << x[i] << " ";
+}
