406ba9d1c0665b856e4f964c88acbfa0087b45ec
Yarik <117121382+demurre@users.noreply.github.com>
2022-11-06 16:03:26 +0000

    Add files via upload

---
+++ practice/WorkingWithStrings/pr10.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    cout << "Enter any symbol: ";
+    string s;
+    char y = 'Y';
+    while (toupper(y) == 'Y') {
+        cin >> y;
+        s += y;
+        cout << "Ще? Y/N ";
+        cin >> y;
+        if (toupper(y) == 'Y') {
+            cout << "Enter the next symbol: ";
+        }
+    }
+    cout << "Number of symbols entered: " << s.size();
+}
